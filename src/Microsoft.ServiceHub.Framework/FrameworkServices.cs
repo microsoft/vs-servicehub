dd413612af5be128e0de4e98415f24021a01adef
Murphy McQuet <murphymcquet@microsoft.com>
2024-11-11 15:46:55 -0800

    Make camel case json converter public

--- src/Microsoft.ServiceHub.Framework/FrameworkServices.cs
+++ src/Microsoft.ServiceHub.Framework/FrameworkServices.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.ServiceHub.Framework.Services;
-using Newtonsoft.Json.Converters;
-using StreamJsonRpc;
 
 namespace Microsoft.ServiceHub.Framework;
 
@@ -19,7 +17,7 @@ public static class FrameworkServices
 	/// This descriptor defines the default protocol used to communicate with an <see cref="IRemoteServiceBroker"/>.
 	/// The moniker is irrelevant because this service is not queried for.
 	/// </remarks>
-	public static readonly ServiceRpcDescriptor RemoteServiceBroker = new CamelCaseTransformingDescriptor(
+	public static readonly ServiceRpcDescriptor RemoteServiceBroker = new NodeToManagedServiceJsonRpcDescriptor(
 		new ServiceMoniker(nameof(RemoteServiceBroker)),
 		ServiceJsonRpcDescriptor.Formatters.UTF8SystemTextJson,
 		ServiceJsonRpcDescriptor.MessageDelimiters.HttpLikeHeaders);
@@ -31,7 +29,7 @@ public static class FrameworkServices
 	/// This descriptor defines the default protocol used to communicate with an <see cref="IAuthorizationService"/>.
 	/// Requests for this service should include client credentials to impersonate a client other than the local process hosting the authorization service.
 	/// </remarks>
-	public static readonly ServiceRpcDescriptor Authorization = new CamelCaseTransformingDescriptor(
+	public static readonly ServiceRpcDescriptor Authorization = new NodeToManagedServiceJsonRpcDescriptor(
 		new ServiceMoniker("Microsoft.ServiceHub.Framework.AuthorizationService"),
 		ServiceJsonRpcDescriptor.Formatters.UTF8SystemTextJson,
 		ServiceJsonRpcDescriptor.MessageDelimiters.HttpLikeHeaders);
@@ -42,79 +40,8 @@ public static class FrameworkServices
 	/// <remarks>
 	/// This descriptor defines the default protocol used to communicate with an <see cref="IBrokeredServiceManifest"/>.
 	/// </remarks>
-	public static readonly ServiceRpcDescriptor RemoteBrokeredServiceManifest = new CamelCaseTransformingDescriptor(
+	public static readonly ServiceRpcDescriptor RemoteBrokeredServiceManifest = new NodeToManagedServiceJsonRpcDescriptor(
 		new ServiceMoniker("Microsoft.VisualStudio.RemoteBrokeredServiceManifest", new Version(0, 2)),
 		ServiceJsonRpcDescriptor.Formatters.UTF8SystemTextJson,
 		ServiceJsonRpcDescriptor.MessageDelimiters.HttpLikeHeaders);
-
-	/// <summary>
-	/// A <see cref="ServiceJsonRpcDescriptor"/> derived type that applies camelCase naming transforms to method and event names
-	/// and trims off any trailing "Async" suffix.
-	/// </summary>
-	private class CamelCaseTransformingDescriptor : ServiceJsonRpcDescriptor
-	{
-		private const string AsyncSuffix = "Async";
-		private static readonly Func<string, string> NameNormalize = name => CommonMethodNameTransforms.CamelCase(name.EndsWith(AsyncSuffix, StringComparison.OrdinalIgnoreCase) ? name.Substring(0, name.Length - AsyncSuffix.Length) : name);
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="CamelCaseTransformingDescriptor"/> class.
-		/// </summary>
-		/// <inheritdoc cref="ServiceJsonRpcDescriptor(ServiceMoniker, Formatters, MessageDelimiters)" />
-		public CamelCaseTransformingDescriptor(ServiceMoniker serviceMoniker, Formatters formatter, MessageDelimiters messageDelimiter)
-			: base(serviceMoniker, formatter, messageDelimiter)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="CamelCaseTransformingDescriptor"/> class.
-		/// </summary>
-		/// <inheritdoc cref="ServiceJsonRpcDescriptor(ServiceJsonRpcDescriptor)"/>
-		public CamelCaseTransformingDescriptor(CamelCaseTransformingDescriptor copyFrom)
-			: base(copyFrom)
-		{
-		}
-
-		/// <inheritdoc />
-		protected internal override JsonRpcConnection CreateConnection(JsonRpc jsonRpc)
-		{
-			JsonRpcConnection connection = base.CreateConnection(jsonRpc);
-			connection.LocalRpcTargetOptions.MethodNameTransform = NameNormalize;
-			connection.LocalRpcTargetOptions.EventNameTransform = NameNormalize;
-			connection.LocalRpcProxyOptions.MethodNameTransform = NameNormalize;
-			connection.LocalRpcProxyOptions.EventNameTransform = NameNormalize;
-			return connection;
-		}
-
-		protected internal override IJsonRpcMessageFormatter CreateFormatter()
-		{
-			IJsonRpcMessageFormatter formatter = base.CreateFormatter();
-
-			// Avoid referencing any MessagePack or Newtonsoft.Json types in this method except when actually taking this code path
-			// by pushing such type references to another method. This defers loading assemblies till they're already in use.
-			switch (formatter)
-			{
-				case JsonMessageFormatter jsonFormatter:
-					ConfigureJsonFormatter(jsonFormatter);
-					break;
-				case SystemTextJsonFormatter stjFormatter:
-					ConfigureJsonFormatter(stjFormatter);
-					break;
-				default:
-					throw new NotSupportedException("Unsupported formatter type: " + formatter.GetType().FullName);
-			}
-
-			return formatter;
-		}
-
-		protected override ServiceRpcDescriptor Clone() => new CamelCaseTransformingDescriptor(this);
-
-		private static void ConfigureJsonFormatter(JsonMessageFormatter jsonFormatter)
-		{
-			jsonFormatter.JsonSerializer.Converters.Add(new VersionConverter());
-		}
-
-		private static void ConfigureJsonFormatter(SystemTextJsonFormatter jsonFormatter)
-		{
-		}
-	}
 }
