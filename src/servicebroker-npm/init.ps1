c5551a8dcf26db14cc78914449480e26b801af82
Andrew Arnott <andrew.arnott@microsoft.com>
2026-02-24 08:49:18 -0700

    Update yarn

--- src/servicebroker-npm/init.ps1
+++ src/servicebroker-npm/init.ps1
@@ -1,3 +1,3 @@
 & "$PSScriptRoot/../../init.ps1"
 dotnet build "$PSScriptRoot/../../test/ServiceBrokerTest"
-node $PSScriptRoot/.yarn/releases/yarn-4.5.0.cjs
+node $PSScriptRoot/.yarn/releases/yarn-4.12.0.cjs
--- src/servicebroker-npm/pack.ps1
+++ src/servicebroker-npm/pack.ps1
@@ -14,16 +14,16 @@ Param(
 Push-Location $PSScriptRoot
 try {
     if ($Restore) {
-        node .yarn/releases/yarn-4.5.0.cjs
+        node .yarn/releases/yarn-4.12.0.cjs
     }
 
-    node .yarn/releases/yarn-4.5.0.cjs build # tsc
+    node .yarn/releases/yarn-4.12.0.cjs build # tsc
     if ($lastexitcode -ne 0) { throw }
 
     dotnet build # sign
     if ($lastexitcode -ne 0) { throw }
 
-    node .yarn/releases/yarn-4.5.0.cjs nbgv-setversion
+    node .yarn/releases/yarn-4.12.0.cjs nbgv-setversion
     if ($lastexitcode -ne 0) { throw }
 
     $Configuration = 'Debug'
@@ -32,10 +32,10 @@ try {
     }
     $OutDir = "../../bin/Packages/$Configuration/npm"
     if (!(Test-Path $OutDir)) { New-Item $OutDir -ItemType Directory }
-    node .yarn/releases/yarn-4.5.0.cjs pack --out $OutDir/%s-%v.tgz
+    node .yarn/releases/yarn-4.12.0.cjs pack --out $OutDir/%s-%v.tgz
     if ($lastexitcode -ne 0) { throw }
 
-    node .yarn/releases/yarn-4.5.0.cjs nbgv-setversion --reset
+    node .yarn/releases/yarn-4.12.0.cjs nbgv-setversion --reset
 }
 finally {
     Pop-Location
